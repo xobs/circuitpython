4af1c2bc516129f28e8347f7b5a36ab38c3f4acf
Sean Cross <sean@xobs.io>
2019-11-27 15:01:43 +0800

    litex: fomu: reset led on startup
    When circuitpython starts, the led is in the `error` state.  Software must
    do something with the LED block to indicate to the user there is no error.
    
    At startup, set the LED to a nice color and slow down the pulsing.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- ports/litex/boards/fomu/board.c
+++ ports/litex/boards/fomu/board.c
@@ -26,8 +26,44 @@
 
 #include "boards/board.h"
 #include "mpconfigboard.h"
+#include "csr.h"
+
+// ICE40 LED Driver hard macro.
+// See http://www.latticesemi.com/-/media/LatticeSemi/Documents/ApplicationNotes/IK/ICE40LEDDriverUsageGuide.ashx?document_id=50668
+enum led_registers {
+    LEDDCR0 = 8,
+    LEDDBR = 9,
+    LEDDONR = 10,
+    LEDDOFR = 11,
+    LEDDBCRR = 5,
+    LEDDBCFR = 6,
+    LEDDPWRR = 1,
+    LEDDPWRG = 2,
+    LEDDPWRB = 3,
+};
+
+#define BREATHE_ENABLE (1 << 7)
+#define BREATHE_EDGE_ON (0 << 6)
+#define BREATHE_EDGE_BOTH (1 << 6)
+#define BREATHE_MODE_MODULATE (1 << 5)
+#define BREATHE_RATE(x) ((x & 7) << 0)
+
+// Write a value into the LEDDA_IP register.
+static void ledda_write(uint8_t value, uint8_t addr) {
+    rgb_addr_write(addr);
+    rgb_dat_write(value);
+}
 
 void board_init(void) {
+    uint8_t onrate = 15;
+    uint8_t offrate = 1;
+
+    ledda_write(BREATHE_ENABLE | BREATHE_MODE_MODULATE | BREATHE_RATE(onrate), LEDDBCRR);
+    ledda_write(BREATHE_ENABLE | BREATHE_MODE_MODULATE | BREATHE_RATE(offrate), LEDDBCFR);
+
+    ledda_write(123, LEDDPWRR); // Red
+    ledda_write(3, LEDDPWRG);   // Green
+    ledda_write(98, LEDDPWRB);  // Blue
 }
 
 bool board_requests_safe_mode(void) {
